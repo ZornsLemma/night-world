8d1b1a9ae356b3af94dbb76136748a37c3f26ca8
Steven Flintham <sgf@lemma.co.uk>
2022-05-28 02:08:24 +0100

    Avoid beebasm "no SAVE command" warning

--- src/disc.asm
+++ src/disc.asm
@@ -1,5 +1,11 @@
-; TODO: we could incbin and save this, to stop beebasm warning
-putfile "orig/boot", "!BOOT", 0, 0
+; We could use putfle to create !BOOT, but by doing this we stop beebasm warning
+; about there being no SAVE command.
+org &0000
+.boot_start
+    incbin "orig/boot"
+.boot_end
+save "!BOOT", boot_start, boot_end
+
 ; TODO: bbc-wor is pure BASIC; ideally we'd rebuild it from ASCII BASIC.
 putfile "orig/bbc-wor", "BBC-Wor", &1900, &1900
 ; TODO: ditto "nightwo"
