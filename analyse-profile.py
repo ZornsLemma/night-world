71b6da7a7e52b033802e68a9d98d9412a6892da7
Steven Flintham <sgf@lemma.co.uk>
2022-05-30 20:38:07 +0100

    WIP

--- analyse-profile.py
+++ analyse-profile.py
@@ -13,11 +13,21 @@ ranges = (
     ("game m/c", 0xd00, 0x8000),
     ("BASIC", 0x8000, 0xc000),
     ("OS", 0xc000, 0x10000),
+    ("OSWORD 9", 0xc735, 0xc748), # .osword9EntryPoint
+    ("OSWORD 9", 0xd839, 0xd85d), # .readPixel
+    ("OSWORD 9", 0xd149, 0xd176), # .plotConvertExternalAbsoluteCoordinatesToPixels
+    ("OSWORD 9", 0xd176, 0xd1ad), # .convertExternalRelativeCoordinateToAbsoluteAndDivideByTwo
+    ("OSWORD 9", 0xd1ad, 0xd1b8), # .signedDivideCoordinateByTwo
+    ("OSWORD 9", 0xd85c, 0xd8ce), # .checkPointXInBoundsAndSetScreenAddresses
+    ("OSWORD 9", 0xd10f, 0xd128), # .checkPointXIsWithinGraphicsWindow
+    ("OSWORD 9", 0xd128, 0xd149), # .checkPointIsWithinWindowHorizontalOrVertical
+    ("OSWORD 9", 0xc750, 0xc759), # .storeAInParameterBlock
 )
+
 range_dict = {}
 for name, start, end in ranges:
     for addr in range(start, end):
-        assert addr not in range_dict
+        #assert addr not in range_dict
         range_dict[addr] = name
 
 region_counts = defaultdict(int)
@@ -26,4 +36,4 @@ for addr, count in c.items():
 
 total_count = sum(x for x in region_counts.values())
 for name, count in region_counts.items():
-    print("%20s %3.1f%% (%8d)" % (name, 100*float(count)/total_count, count))
+    print("%-10s %5.1f%% (%8d)" % (name, 100*float(count)/total_count, count))
