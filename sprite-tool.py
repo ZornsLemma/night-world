2c6f70afca8f75a6ffcc4d2c24591f40a5521d34
Steven Flintham <sgf@lemma.co.uk>
2022-05-30 16:49:29 +0100

    WIP

--- sprite-tool.py
+++ sprite-tool.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageColor
+from PIL import Image, ImageColor, ImageDraw
 
 # It looks like the screen dumps are not quite properly aligned, so work
 # around that.
@@ -8,6 +8,8 @@ sprite_sheet_x_spacing = 96
 sprite_sheet_y_spacing = 32
 sprite_image_width = 48
 sprite_image_height = 32
+enlarge_factor = 4
+grid_colour = ImageColor.getrgb("grey")
 
 def get_sprite_frames(sprite_sheet, n):
     frames = []
@@ -23,6 +25,24 @@ def get_sprite_frames(sprite_sheet, n):
 def save_sprite_frames(sprite_frames, basename):
     for i, frame in enumerate(sprite_frames):
         frame.save(basename + "-%d.png" % i, "PNG")
+
+def enlarge_frame(frame):
+    assert frame.size == (sprite_image_width, sprite_image_height)
+    enlarged = frame.resize((frame.size[0] * enlarge_factor, frame.size[1] * enlarge_factor), Image.NEAREST)
+    pixel_width = enlarged.size[0] / 12
+    pixel_height = enlarged.size[1] / 16
+    draw = ImageDraw.Draw(enlarged)
+    for pixel_x in range(1, 12):
+        draw.line((pixel_x * pixel_width, 0, pixel_x * pixel_width, enlarged.size[1]),
+                  fill=grid_colour)
+    for pixel_y in range(1, 16):
+        draw.line((0, pixel_y * pixel_height, enlarged.size[0], pixel_y * pixel_height),
+                  fill=grid_colour)
+    return enlarged
+
+def save_sprite_frames_large(sprite_frames, basename):
+    for i, frame in enumerate(sprite_frames):
+        enlarge_frame(frame).save(basename + "-%d-large.png" % i, "PNG")
 
 def save_sprite_animation(sprite_frames, basename):
     assert len(sprite_frames) == 4
@@ -44,6 +64,7 @@ def save_sprite(sprite_frames, n):
     assert len(sprite_frames) == 4
     basename = "img/sprite-%02d" % n
     save_sprite_frames(sprite_frames, basename)
+    save_sprite_frames_large(sprite_frames, basename)
     save_sprite_animation(sprite_frames, basename)
 
 for n in range(19):
